5503e0e7ddceb3f33823e2e3c75ff4e7706400d0
awepp-jiappa <103619644+awepp-jiappa@users.noreply.github.com>
2026-06-10 14:11:59 +0900

    Align shared JVM target to 17

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -10,4 +10,13 @@ android {
     defaultConfig {
         minSdk = 26
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
