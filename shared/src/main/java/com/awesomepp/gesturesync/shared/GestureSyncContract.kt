34833e89c7322b1ed8bc100db1781fb832c4378f
awepp-jiappa <103619644+awepp-jiappa@users.noreply.github.com>
2026-06-10 13:29:51 +0900

    Add shared gesture contract

---
+++ shared/src/main/java/com/awesomepp/gesturesync/shared/GestureSyncContract.kt
@@ -0,0 +1,12 @@
+package com.awesomepp.gesturesync.shared
+
+object GestureSyncContract {
+    const val PATH_OPEN_PHONE_APP = "/gesturesync/open-phone-app"
+    const val PATH_SWIPE = "/gesturesync/swipe"
+    const val PATH_TAP = "/gesturesync/tap"
+
+    const val DIRECTION_UP = "UP"
+    const val DIRECTION_DOWN = "DOWN"
+    const val DIRECTION_LEFT = "LEFT"
+    const val DIRECTION_RIGHT = "RIGHT"
+}
