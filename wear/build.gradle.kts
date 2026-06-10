cf4f6c4772a1b6ae7a75cc158120ca75ce3cf73e
awepp-jiappa <103619644+awepp-jiappa@users.noreply.github.com>
2026-06-10 13:32:23 +0900

    Add wear module Gradle config

---
+++ wear/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.awesomepp.gesturesync.wear"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.awesomepp.gesturesync.wear"
+        minSdk = 30
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
+
+dependencies {
+    implementation(project(":shared"))
+    implementation("com.google.android.gms:play-services-wearable:18.2.0")
+}
