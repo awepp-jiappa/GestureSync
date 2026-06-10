e502eb8390e9c51233ab0f1fa16b55fe4437447a
awepp-jiappa <103619644+awepp-jiappa@users.noreply.github.com>
2026-06-10 13:29:27 +0900

    Add shared module

---
+++ shared/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.awesomepp.gesturesync.shared"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 26
+    }
+}
