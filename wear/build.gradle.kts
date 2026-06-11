0bc4b20c874b044b1af431d1fb3ce6072874457b
awepp-jiappa <103619644+awepp-jiappa@users.noreply.github.com>
2026-06-12 01:07:22 +0900

    Restore wear module dependencies

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -17,10 +17,3 @@ android {
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-}
