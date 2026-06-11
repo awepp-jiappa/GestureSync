8a72275c008e2504b8f54addf310de0061342be4
awepp-jiappa <103619644+awepp-jiappa@users.noreply.github.com>
2026-06-12 00:46:37 +0900

    Complete wear Gradle dependencies

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -17,13 +17,4 @@ android {
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-}
-
-dependencies {
-    implementation
+        targetCompatibility = JavaVersion.VERSION_
